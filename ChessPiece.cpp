74a24fa442f4453404a8aa817999cbf07803ca79
Lukas Bos <mrlukasbos@gmail.com>
2017-12-03 15:09:30 +0100

    add simple Rook moveset

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -9,21 +9,13 @@ ChessPiece::ChessPiece(Board *board, GridElement *location, PieceColor color, Pi
                                                                                                 location(location),
                                                                                                 color(color),
                                                                                                 type(type) {
-
     // for the finding images
     std::string names[] = {"King", "Queen", "Rook", "Bishop", "Knight", "Pawn"};
-    if (color == BLACK) {
-        imageUrlPrefix = "Black";
-    } else {
-        imageUrlPrefix = "White";
-    }
-
+    imageUrlPrefix = color == BLACK ? "Black" : "White";
     img.loadFromFile("images/" + imageUrlPrefix + names[type] + ".png");
 }
 
 void ChessPiece::drawChessPiece(sf::RenderWindow &window) {
-
-    //It's an example, you have to keep the sf::Texture and sf::Sprite as members of your class
     texture.loadFromImage(img);
     sprite.setTexture(texture, true);
     sprite.setPosition(sf::Vector2f(location->posX + 10, location->posY + 10)); // absolute position
@@ -33,26 +25,24 @@ void ChessPiece::drawChessPiece(sf::RenderWindow &window) {
 std::vector<GridElement *> ChessPiece::getAvailableMoves() {
     std::vector<GridElement *> availableMoves;
 
-    bool isBottomColor = board->bottomColor == color;
-    int direction = isBottomColor ? -1 : 1;
-    availableMoves.push_back(board->elements[location->coordinates.x][location->coordinates.y + (1 * direction)]);
-    if (!hasMoved) {
-        availableMoves.push_back(board->elements[location->coordinates.x][location->coordinates.y + (2 * direction)]);
-    }
+    if (type == PAWN) {
+        bool isBottomColor = board->bottomColor == color;
+        int direction = isBottomColor ? -1 : 1;
+        availableMoves.push_back(board->elements[location->coordinates.x][location->coordinates.y + (1 * direction)]);
+        if (!hasMoved) {
+            availableMoves.push_back(
+                    board->elements[location->coordinates.x][location->coordinates.y + (2 * direction)]);
+        }
+    } else if (type == ROOK) {
+        for (int i = 0; i < 8; i++) {
+            if (location->coordinates.y != i) {
+                availableMoves.push_back(board->elements[location->coordinates.x][i]);
+            }
+            if (location->coordinates.x != i) {
+                availableMoves.push_back(board->elements[i][location->coordinates.y]);
+            }
+        }
 
+    }
     return availableMoves;
 }
-
-
-/*
- *
- * Let's decide the movesets for each piece here.
- * Let's start with pawn.
- * pawns of bottom color move up
- * pawns of upper color move down
- *
- * What we need in this method:
- *  - Type
- *  - Color
- *  - What color is at the bottom
- */
