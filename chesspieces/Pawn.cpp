992d83131f55b2193145255b67e22a1b3539a4e5
Lukas Bos <mrlukasbos@gmail.com>
2017-12-05 14:31:47 +0100

    update pawn behavour: no capture diagonally and check if pieces are in the way

--- chesspieces/Pawn.cpp
+++ chesspieces/Pawn.cpp
@@ -21,10 +21,29 @@ std::vector<GridElement *> Pawn::getAvailableMoves() {
 
     bool isBottomColor = board->bottomColor == color;
     short direction = isBottomColor ? -1 : 1;
-    availableMoves.push_back(board->elements[x][y + (1 * direction)]);
-    if (!hasMoved) {
-        availableMoves.push_back(
-                board->elements[x][y + (2 * direction)]);
+
+
+    GridElement *oneForward = board->elements[x][y + (1 * direction)];
+    GridElement *twoForward = board->elements[x][y + (2 * direction)];
+
+    if (!oneForward->chessPiece) {
+        availableMoves.push_back(oneForward);
+
+        if (!twoForward->chessPiece && !hasMoved) {
+            availableMoves.push_back(twoForward);
+        }
+    }
+
+
+    GridElement *diagonalup1 = board->elements[x + 1][y + (1 * direction)];
+    GridElement *diagonalup2 = board->elements[x - 1][y + (1 * direction)];
+
+    if (diagonalup1->chessPiece && diagonalup1->chessPiece->color != color) {
+        availableMoves.push_back(diagonalup1);
+    }
+
+    if (diagonalup2->chessPiece && diagonalup2->chessPiece->color != color) {
+        availableMoves.push_back(diagonalup2);
     }
     return availableMoves;
 }
